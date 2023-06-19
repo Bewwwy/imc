8bffdd4ae4b0e42875f833fcad7d99a55ebb4677
Bewwwy <106964692+Bewwwy@users.noreply.github.com>
2023-06-19 12:29:58 -0300

    o:

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Capturar os dados do forms
+const dados = document.querySelector('#f');
+
+dados.addEventListener('submit', function(event) {
+    event.preventDefault():
+    console.log('Tente denovo')
+});
